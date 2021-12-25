158575e5611b5e7397b4d521c3717c921433a220
Prakhar Srivastava <prakhar.19982015@outlook.com>
2021-12-25 19:08:59 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-  return (
-    <div >
-      Phone Directory     
-    </div>
-  );
+    return (
+      <div>
+        {/* <div className="header">
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span> <br />
+          <span> Phone </span>
+        </div> */}
+        <label htmlFor="name">Name : </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Prakhar"/>
+      </div>
+    );
   }
 }
 
